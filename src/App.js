1fce18f38bdbf09d71b116c3307e5e17cdef4790
sahil937 <meharsahil207@gmail.com>
2022-03-13 21:39:40 -0500

    StudentList is imported

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import {React} from 'react';
 import './App.css';
+import {DataProvider} from "./DataContext";
+import StudentList from "./Components/StudentList"
+
+
 
 function App() {
+
   return (
+    <DataProvider>
     <div >
 
+      <StudentList />
+
+
     </div>
+
+    </DataProvider>
   );
 }
 
