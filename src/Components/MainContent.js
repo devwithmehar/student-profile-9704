4e4dfa5ff0046d772da2b19c094313bae73098ea
sahil937 <meharsahil207@gmail.com>
2022-03-15 13:26:28 -0500

    MainContent is created

---
+++ src/Components/MainContent.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import StudentList from './StudentList'
+
+
+const MainContent = () => {
+  return (
+    <div className='mainContent'>
+
+    <div className='scrollArea'>
+       <StudentList />
+
+    </div>
+
+    </div>
+  )
+}
+
+export default MainContent
